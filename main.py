7f0129cc64e4cb20f6b21546aead1780028ed600
Jaedan <jaedanparsons@gmail.com>
2024-08-28 09:43:30 -0800

    Added the database.db and added the data to the table

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QGridLayout, \
     QLineEdit, QPushButton, QLabel, QWidget, QComboBox ,QMainWindow, \
-    QTableWidget
+    QTableWidget, QTableWidgetItem
 
 from PySide6.QtGui import QAction
 
 import sys
-
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,10 +26,23 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(('Id', 'Name', 'Course', 'Mobile'))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
+
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        #print(list(result)) #How to print the results to see\
+
+        self.table.setRowCount(0) # Refresh the cursor to the beginning
+
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+        connection.close()
 
 
 
@@ -37,4 +50,5 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 speed_calculator = MainWindow()
 speed_calculator.show()
+speed_calculator.load_data()
 sys.exit(app.exec())
