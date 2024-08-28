1ffb66a33080f3c157bf09f1852c098525e118a6
Jaedan <jaedanparsons@gmail.com>
2024-08-28 12:09:49 -0800

    Added the add_student functionality so you can add students

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QGridLayout, \
-    QLineEdit, QPushButton, QLabel, QWidget, QComboBox ,QMainWindow, \
-    QTableWidget, QTableWidgetItem
+    QLineEdit, QPushButton, QLabel, QWidget, QComboBox, QMainWindow, \
+    QTableWidget, QTableWidgetItem, QDialog
 
 from PySide6.QtGui import QAction
 
 import sys
 import sqlite3
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,25 +18,25 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
 
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
-        #Create Table
+        # Create Table
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(('Id', 'Name', 'Course', 'Mobile'))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
-
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
         #print(list(result)) #How to print the results to see\
 
-        self.table.setRowCount(0) # Refresh the cursor to the beginning
+        self.table.setRowCount(0)  # Refresh the cursor to the beginning
 
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
@@ -44,11 +45,60 @@ class MainWindow(QMainWindow):
 
         connection.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
 
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # name box
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add Combo box of courses
+        self.course_name = QComboBox()
+        courses = ['Biology', 'Math', 'Astronomy', 'Physics']
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # add mobile widget
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        # Add submit button
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()  # need to do to save the changes
+        cursor.close()
+        connection.close()
+
+        main_window.load_data() # reload updated data
 
 
 app = QApplication(sys.argv)
-speed_calculator = MainWindow()
-speed_calculator.show()
-speed_calculator.load_data()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
